4771c2a8d0524d4bf77f0520342ac7bfbe1037d7
Anfas PV <anfaspv.info@gmail.com>
2022-10-29 22:58:19 +0400

    loading

--- main.js
+++ main.js
@@ -1,38 +1,42 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: "./assets/icons/Icon_256x256.png",
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      nodeIntegration: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
   // check if internet is available, if not, load local html file (./renderer/offline.html),file:///home/sanford/Downloads/loading-screen-animation/dist/index.html
 
-  // else load the online html file (./renderer/loading.html) and load https://sanfordcorp.co 
+  // else load the online html file (./renderer/loading.html) and load https://sanfordcorp.co
   // in the background, once it is loaded, load the url in the main window
-  win.loadFile('./renderer/loading.html')
+  win.loadFile("./renderer/loading.html");
   // if internet is connected, load the online.html file
-  win.webContents.on('did-finish-load', () => {
-    win.webContents.executeJavaScript(`window.location.href = 'https://sanfordcorp.co'`)
-  })
-
+  win.webContents.on("did-finish-load", () => {
+    win.webContents.executeJavaScript(
+      `window.location.href = 'https://sanfordcorp.co'`
+    );
+  });
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
